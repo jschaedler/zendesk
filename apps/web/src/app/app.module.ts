075c3f2bc6bb83d1b86035b3a085ca258d2d5a12
jschaedler <jschaedler7@gmail.com>
2021-11-26 18:07:43 -0500

    add app routing

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,9 +5,24 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule } from '@angular/router';
+
+// Components
+import { AuthComponent } from './components/auth/auth.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, TicketsComponent, AuthComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'auth', component: AuthComponent, pathMatch: 'full' },
+      { path: 'tickets', component: TicketsComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: 'tickets' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
